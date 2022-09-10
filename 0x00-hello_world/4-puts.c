97556e1a32e22a8bc7052f307ba50bb8f1616f08
Jay <111271226+Kenzijay@users.noreply.github.com>
2022-09-10 05:17:16 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry point
-*
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-	char c = 'a';
-
-	while (c <= 'z')
-	{
-		if (c != 'q' && c != 'e')
-			putchar(c);
-
-		c++;
-	}
-	putchar('\n');
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
