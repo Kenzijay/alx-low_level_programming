9a0cac0681a7a0f1675cbfee7ddf2eca1dd118ba
Jay <111271226+Kenzijay@users.noreply.github.com>
2022-09-15 13:27:18 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
 
-int _putchar(char);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,11 +11,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void tiles_table(void)
-int add(int a, int b);
+void times_table(void);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r, int n);
 
 #endif
-^C
